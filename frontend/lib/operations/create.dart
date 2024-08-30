676d3ede0d9be7a9d6fca05c8c3d8976b3621cbe
Muhammad Zeeshan Akram <161469971+Zeeshansiddiqui9790@users.noreply.github.com>
2024-08-31 01:13:16 +0500

    initial setup

---
+++ frontend/lib/operations/create.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CreateOperation extends StatefulWidget {
+  const CreateOperation({super.key});
+
+  @override
+  State<CreateOperation> createState() => _CreateOperationState();
+}
+
+class _CreateOperationState extends State<CreateOperation> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
